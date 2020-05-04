9de4b48bce9f0e88d2495a75c8ff10cde10bd984
John Long <jlcomp45@gmail.com>
2020-05-04 10:19:43 -0500

    completed phase one of heist

---
+++ Member.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace bank_heist
+{
+    public class Member
+    {
+        public string Name { get; set; }
+
+        public int skillLevel { get; set; }
+         
+        public double courage { get; set; }
+    }
+}
